66c2fb5a288705b050e8ae655bb51f3c87fb2a06
dmitrykurmanov <kurmanov.work@gmail.com>
2019-09-03 15:01:09 +0300

    fixed tests

--- tests/vizualizerBase.ts
+++ tests/vizualizerBase.ts
@@ -2,16 +2,16 @@ import { VisualizerBase } from "../src/visualizerBase";
 
 test("custom colors", () => {
   expect(new VisualizerBase(null, null, null).getColors(1)).toEqual([
-    "86e1fb",
-    "3999fb",
-    "ff6771",
-    "1eb496",
-    "ffc152",
-    "aba1ff",
-    "7d8da5",
-    "4ec46c",
-    "cf37a6",
-    "4e6198"
+    "#86e1fb",
+    "#3999fb",
+    "#ff6771",
+    "#1eb496",
+    "#ffc152",
+    "#aba1ff",
+    "#7d8da5",
+    "#4ec46c",
+    "#cf37a6",
+    "#4e6198"
   ]);
 
   VisualizerBase.customColors = ["red", "green", "blue"];
