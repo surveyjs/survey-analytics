68080b027fe04a602848a02528835311b915c31c
dmitrykurmanov <kurmanov.work@gmail.com>
2020-04-20 23:55:04 +0300

    implemented showColumnDropdown

--- src/tables/tabulator.ts
+++ src/tables/tabulator.ts
@@ -1,6 +1,7 @@
 import { Table } from "./table";
 import { SurveyModel } from "survey-core";
 import { ColumnVisibility, QuestionLocation } from "./config";
+import { localization } from "../localizationManager";
 
 import "./tabulator.scss";
 
@@ -32,24 +33,117 @@ export class Tabulator extends Table {
   }
 
   private tabulatorTables: any = null;
+  private toolsContainer: HTMLElement = null;
+  private tableContainer: HTMLElement = null;
 
   public render = () => {
     const columns = this.getColumns();
-
     const data = this.tableData;
 
-    this.targetNode.innerHTML = "";
+    this.toolsContainer = document.createElement("div");
+    this.tableContainer = document.createElement("div");
 
-    this.tabulatorTables = new TabulatorTables(this.targetNode, {
+    this.targetNode.innerHTML = "";
+    this.targetNode.appendChild(this.toolsContainer);
+    this.targetNode.appendChild(this.tableContainer);
+
+    this.tabulatorTables = new TabulatorTables(this.tableContainer, {
       data,
       layout: "fitColumns",
+      pagination: "local",
+      paginationSize: 5,
+      movableColumns: true,
       columns,
     });
+
+    this.renderTools();
+  };
+
+  renderTools = () => {
+    const toolsContainer = this.toolsContainer;
+    const showColumnDropdown = this.getShowColumnDropdown();
+
+    toolsContainer.innerHTML = "";
+
+    toolsContainer.appendChild(showColumnDropdown);
+  };
+
+  getShowColumnDropdown = (): HTMLElement => {
+    const dropdown = document.createElement("select");
+
+    var hiddenColumns = this.columns.filter(
+      (column) => column.visibility === ColumnVisibility.Invisible
+    );
+
+    var option = document.createElement("option");
+    option.text = localization.getString("showColumn");
+    option.disabled = true;
+    option.selected = true;
+    dropdown.appendChild(option);
+
+    hiddenColumns.forEach((column) => {
+      var option = document.createElement("option");
+      var text = column.displayName;
+      if (text.length > 20) {
+        text = text.substring(0, 20) + "...";
+      }
+      option.text = text;
+      option.title = column.displayName;
+      option.value = column.name;
+      dropdown.appendChild(option);
+    });
+
+    var self = this;
+    dropdown.onchange = function (e: any) {
+      const val = e.target.value;
+      e.stopPropagation();
+      if (!val) return;
+
+      var column = self._columns.filter((column) => column.name === val)[0];
+      column.visibility = ColumnVisibility.Visible;
+      self.tabulatorTables.toggleColumn(column.name);
+      self.update();
+    };
+
+    return dropdown;
   };
 
   public destroy = () => {
     this.tabulatorTables.destroy();
     this.targetNode.innerHTML = "";
+  };
+
+  public update() {
+    this.renderTools();
+  }
+
+  protected getTitleFormatter = (
+    cell: any,
+    formatterParams: any,
+    onRendered: any,
+    columnName: any
+  ) => {
+    //build dropdown
+    var hideColumnBtn = document.createElement("button");
+    hideColumnBtn.style.display = "block";
+    hideColumnBtn.innerHTML = "<span> hide </span>";
+    hideColumnBtn.onclick = () => {
+      this._columns.filter(
+        (column) => column.name === columnName
+      )[0].visibility = ColumnVisibility.Invisible;
+      this.tabulatorTables.toggleColumn(columnName);
+      this.update();
+    };
+
+    //set header title
+    var title = document.createElement("div");
+    title.style.height = "50px";
+    title.innerHTML = cell.getValue();
+
+    //add menu to title
+    title.appendChild(hideColumnBtn);
+
+    return title;
   };
 
   protected getColumns = () => {
@@ -60,6 +154,15 @@ export class Tabulator extends Table {
         field: column.name,
         title: (question && question.title) || column.displayName,
         visible: column.visibility !== ColumnVisibility.Invisible,
+        headerFilter: true,
+        titleFormatter: (cell: any, formatterParams: any, onRendered: any) => {
+          return this.getTitleFormatter(
+            cell,
+            formatterParams,
+            onRendered,
+            column.name
+          );
+        },
       };
     });
 
