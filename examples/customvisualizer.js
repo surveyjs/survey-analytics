8e931436357738eaca7eb07654b836f8dec23dae
tsv2013 <tsv2013@users.noreply.github.com>
2020-06-29 17:33:09 +0300

    Added custom visualizers sample for the #37 - Support visualization for custom widgets

---
+++ examples/customvisualizer.js
@@ -0,0 +1,157 @@
+// Register the custom question type
+// Lets the question collects two values - min and max
+// Question value is an object like { min: 3, max: 9 }
+Survey.Serializer.addClass("custom-question", [], null, "text");
+
+// Custom visualizer finds min and max values across all the answers on this question and shows them
+function CustomVisualizer(targetElement, question, data) {
+  var self = this;
+  self.targetElement = targetElement;
+  self.question = question;
+  self.data = data;
+  self.toolbarItemCreators = {};
+
+  var getData = function() {
+    var result = [Number.MAX_VALUE, Number.MIN_VALUE];
+
+    this.data.forEach(function(row) {
+      const rowValue = row[self.question.name];
+      if (!!rowValue) {
+        if(rowValue.min < result[0]) {
+          result[0] = rowValue.min;
+        }
+        if(rowValue.max > result[1]) {
+          result[1] = rowValue.max;
+        }
+      }
+    });
+
+    return result;
+  };
+
+  var renderContent = function(contentContainer) {
+    var data2render = getData();
+    var minEl = document.createElement("div");
+    var minTextEl = document.createElement("span");
+    minTextEl.innerText = "Min: ";
+    var minValEl = document.createElement("span");
+    minValEl.innerText = data2render[0];
+    minEl.appendChild(minTextEl);
+    minEl.appendChild(minValEl);
+    contentContainer.appendChild(minEl);
+    var maxEl = document.createElement("div");
+    var maxTextEl = document.createElement("span");
+    maxTextEl.innerText = "Max: ";
+    var maxValEl = document.createElement("span");
+    maxValEl.innerText = data2render[1];
+    maxEl.appendChild(maxTextEl);
+    maxEl.appendChild(maxValEl);
+    contentContainer.appendChild(maxEl);
+  };
+
+  return {
+    name: "minMaxVisualizer",
+    render: function(targetElement) {
+      self.targetElement = targetElement || self.targetElement;
+  
+      var toolbarNodeContainer = document.createElement("div");
+      var contentContainer = document.createElement("div");
+      contentContainer.className = "sa-visualizer__content";
+  
+      renderContent(contentContainer);
+  
+      var toolbar = document.createElement("div");
+      toolbar.className = "sva-toolbar";
+      toolbarNodeContainer.appendChild(toolbar);
+      SurveyAnalytics.VisualizerBase.prototype.createToolbarItems.apply(self, [toolbar]);
+
+      self.targetElement.appendChild(toolbarNodeContainer);
+      self.targetElement.appendChild(contentContainer);
+    },
+    registerToolbarItem: function(itemName, creator) {
+        SurveyAnalytics.VisualizerBase.prototype.registerToolbarItem.apply(self, [itemName, creator]);
+    },
+    update: function(data) {
+        self.data = data;
+    },
+    destroy: function() {
+        self.targetElement.innerHTML = "";
+    }
+  };
+}
+
+// Register custom visualizer for the given question type
+SurveyAnalytics.VisualizationManager.registerVisualizer("custom-question", CustomVisualizer);
+// Set localized title of this visualizer
+SurveyAnalytics.localization.locales["en"]["visualizer_minMaxVisualizer"] = "Min/Max Values";
+
+// Custom visualizer finds min value across all the answers on this question and shows it
+function CustomMinVisualizer(targetElement, question, data) {
+    var self = this;
+    self.targetElement = targetElement;
+    self.question = question;
+    self.data = data;
+    self.toolbarItemCreators = {};
+  
+    var getData = function() {
+      var result = Number.MAX_VALUE;
+  
+      this.data.forEach(function(row) {
+        const rowValue = row[self.question.name];
+        if (!!rowValue) {
+          if(rowValue.min < result) {
+            result = rowValue.min;
+          }
+        }
+      });
+  
+      return result;
+    };
+  
+    var renderContent = function(contentContainer) {
+      var data2render = getData();
+      var minEl = document.createElement("div");
+      var minTextEl = document.createElement("span");
+      minTextEl.innerText = "Min: ";
+      var minValEl = document.createElement("span");
+      minValEl.innerText = data2render;
+      minEl.appendChild(minTextEl);
+      minEl.appendChild(minValEl);
+      contentContainer.appendChild(minEl);
+    };
+  
+    return {
+      name: "minVisualizer",
+      render: function(targetElement) {
+        self.targetElement = targetElement || self.targetElement;
+    
+        var toolbarNodeContainer = document.createElement("div");
+        var contentContainer = document.createElement("div");
+        contentContainer.className = "sa-visualizer__content";
+    
+        renderContent(contentContainer);
+    
+        var toolbar = document.createElement("div");
+        toolbar.className = "sva-toolbar";
+        toolbarNodeContainer.appendChild(toolbar);
+        SurveyAnalytics.VisualizerBase.prototype.createToolbarItems.apply(self, [toolbar]);
+  
+        self.targetElement.appendChild(toolbarNodeContainer);
+        self.targetElement.appendChild(contentContainer);
+      },
+      registerToolbarItem: function(itemName, creator) {
+          SurveyAnalytics.VisualizerBase.prototype.registerToolbarItem.apply(self, [itemName, creator]);
+      },
+      update: function(data) {
+        self.data = data;
+      },
+      destroy: function() {
+        self.targetElement.innerHTML = "";
+      }
+    };
+}
+
+// Register the second custom visualizer for the given question type
+SurveyAnalytics.VisualizationManager.registerVisualizer("custom-question", CustomMinVisualizer);
+// Set localized title of this visualizer
+SurveyAnalytics.localization.locales["en"]["visualizer_minVisualizer"] = "Min Value Only";
