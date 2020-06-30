757cdad562c5098178ba10bf878442cd1f9e6775
tsv2013 <tsv2013@users.noreply.github.com>
2020-06-30 18:24:43 +0300

    Unit test added

--- tests/boolean.test.ts
+++ tests/boolean.test.ts
@@ -1,4 +1,4 @@
-import { QuestionBooleanModel } from "survey-core";
+import { QuestionBooleanModel, SurveyModel } from "survey-core";
 import { BooleanPlotly } from "../src/plotly/boolean";
 
 let boolean: BooleanPlotly;
@@ -35,4 +35,38 @@ test("getLabels method", () => {
 
 test("getData method", () => {
   expect(boolean.getData()).toEqual([[3, 1]]);
+});
+
+test("getData localized", () => {
+  var survey = new SurveyModel({
+    "pages": [
+      {
+        "name": "page1",
+        "elements": [
+          {
+            "type": "boolean",
+            "name": "bool",
+            "title": "Are you 21 or older?",
+            "label": "Are you 21 or older?",
+            "labelTrue": "Oui",
+            "labelFalse": "Non"
+          }
+        ]
+      }
+    ]
+  });
+  survey.locale = "fr";
+  var firstResult = { "bool":true, "organization_type":"Hobbyist" };
+  var secondResult = { "bool":false, "organization_type":"In-house" };
+  var test = [firstResult, secondResult];
+  var normalizedData = test.map(function (item) {
+    survey.getAllQuestions().forEach(function (q) {
+        if (item[q.name] === undefined) {
+          item[q.name] = "";
+        }
+    });
+    return item;
+  });
+  boolean = new BooleanPlotly(null, survey.getQuestionByName("bool"), normalizedData, {});
+  expect(boolean.getData()).toEqual([[1, 1]]);
 });
