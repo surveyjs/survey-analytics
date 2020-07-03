c93c275ca835012acc10a8878e2a81d54840ad08
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2020-07-03 19:59:26 +0300

    Fix getColumns test

--- tests/tables/tabulator.test.ts
+++ tests/tables/tabulator.test.ts
@@ -75,6 +75,6 @@ test("getColumns method", () => {
   const columns = <any>tabulator["getColumns"]();
 
   expect(JSON.stringify(columns)).toBe(
-    '[{\"field\":\"\",\"title\":\"\"},{\"field\":\"car\",\"title\":\"What car are you driving?\",\"visible\":true,\"headerFilter\":true}]'
+    '[{"field":"","title":"","download":false,"resizable":false},{"field":"car","title":"What car are you driving?","minWidth":155,"width":208,"visible":true,"headerFilter":true}]'
   );
 });
