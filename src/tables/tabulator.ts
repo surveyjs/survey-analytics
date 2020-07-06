3e3b93f9909f916d3d834ae7fd66b5eadc93342c
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2020-07-06 16:03:55 +0300

    Delete unnecessary methods

--- src/tables/tabulator.ts
+++ src/tables/tabulator.ts
@@ -186,7 +186,6 @@ export class Tabulator extends Table {
     return title;
   };
 
-  //actions for columns
   getHeaderActions = (columnName: string): HTMLDivElement => {
     const container = document.createElement("div");
     container.classList.add("sa-tabulator__action-container");
@@ -194,54 +193,6 @@ export class Tabulator extends Table {
     columnActions.render();
     return container;
   };
-
-  getDragBtn(): HTMLButtonElement {
-    const btn = document.createElement("button");
-    btn.className = "sa-tabulator__svg-button sa-tabulator__drag-button";
-    btn.appendChild(ActionsHelper.createSvgElement("drag"));
-    btn.onclick = (e) => {
-      e.stopPropagation();
-    };
-    return btn;
-  }
-
-  getSortBtn(): HTMLButtonElement {
-    const descTitle = localization.getString("descOrder");
-    const ascTitle = localization.getString("ascOrder");
-    var btn = ActionsHelper.createSvgButton("sorting");
-    btn.onclick = (e) => {
-      btn.title = btn.title == ascTitle ? descTitle : ascTitle;
-    };
-    btn.ondrag = (e) => {
-      e.stopPropagation();
-    };
-    return btn;
-  }
-
-  getHideBtn(columnName: string): HTMLButtonElement {
-    var btn = ActionsHelper.createSvgButton("hide");
-    btn.onclick = () => {
-      this._columns.filter(
-        (column) => column.name === columnName
-      )[0].visibility = ColumnVisibility.Invisible;
-      this.tabulatorTables.toggleColumn(columnName);
-      this.update();
-    };
-    return btn;
-  }
-
-  getMoveToDetailsBtn(columnName: string): HTMLButtonElement {
-    const button = ActionsHelper.createSvgButton("movetodetails");
-    button.title = localization.getString("moveToDetail");
-    button.onclick = (e) => {
-      e.stopPropagation();
-      this._columns.filter((column) => column.name === columnName)[0].location =
-        QuestionLocation.Row;
-      this.tabulatorTables.toggleColumn(columnName);
-      this.update();
-    };
-    return button;
-  }
 
   getEntriesContainer(): HTMLElement {
     const selectorContainer = document.createElement("div");
