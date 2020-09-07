b4403dad3817ca8514d178277396328daa6237e3
dmitrykurmanov <kurmanov.work@gmail.com>
2020-09-07 17:20:30 +0300

    fix for the https://surveyjs.answerdesk.io/internal/ticket/details/T5111

--- tests/wordcloud.test.ts
+++ tests/wordcloud.test.ts
@@ -4,12 +4,13 @@ import { WordCloud } from "../src/wordcloud/wordcloud";
 test("remove stopwords", () => {
   var wc = new WordCloud(new QuestionCommentModel("q1"), [
     { q1: "The Thimes" },
-    { q1: "Super mega answer mega" }
+    { q1: "mega mega Super answer" },
   ]);
   var data = wc.getData();
-  expect(Object.keys(data).length).toEqual(4);
-  expect(data.filter(d => d[0] === "The").length).toEqual(0);
-  expect(data.filter(d => d[0] === "mega").length).toEqual(1);
-  expect(data.filter(d => d[0] === "mega")[0][0]).toEqual("mega");
-  expect(data.filter(d => d[0] === "mega")[0][1]).toEqual(2);
+  debugger;
+  expect(Object.keys(data).length).toEqual(3);
+  expect(data.filter((d) => d[0] === "The").length).toEqual(0);
+  expect(data.filter((d) => d[0] === "mega").length).toEqual(1);
+  expect(data.filter((d) => d[0] === "mega")[0][0]).toEqual("mega");
+  expect(data.filter((d) => d[0] === "mega")[0][1]).toEqual(2);
 });
