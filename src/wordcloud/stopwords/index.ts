58b50a3d4a9e5af37612d1d9708402792b994ac5
Serge Sobchuk <serge.sobchuk@gmail.com>
2020-11-30 16:33:55 +0100

    Dutch localization

--- src/wordcloud/stopwords/index.ts
+++ src/wordcloud/stopwords/index.ts
@@ -1,9 +1,11 @@
 import { stopWords } from "./english";
 import { stopWords as noStopWords } from "./norwegian";
+import { stopWords as nlStopWords } from "./dutch";
 
 var stopWordsDictionary: { [index: string]: Array<string> } = {};
 stopWordsDictionary["en"] = stopWords;
 stopWordsDictionary["no"] = noStopWords;
+stopWordsDictionary["nl"] = nlStopWords;
 
 export var textHelper = {
   getStopWords: (locale: string = "") => {
