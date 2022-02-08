c19e84057dd728639ff7838e640c727e84db4933
T_S_V <tsv2013@noreply.github.com>
2022-02-08 11:07:04 +0300

    Added debug example page

---
+++ examples/debug.js
@@ -0,0 +1,20 @@
+
+// var json = JSON.parse(jsonStr);
+var json = {};
+var survey = new Survey.SurveyModel(json);
+
+var data = [];
+
+var visPanel = new SurveyAnalytics.VisualizationPanel(
+  survey.getAllQuestions(),
+  data,
+  {
+    allowChangeAnswersOrder: true,
+    allowShowPercentages: true,
+    allowHideEmptyAnswers: false,
+    allowTransposeData: false,
+    allowTopNAnswers: true,
+  }
+);
+visPanel.showHeader = true;
+visPanel.render(document.getElementById("summaryContainer"));
