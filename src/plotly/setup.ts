86c5e9f5ff7d4c77d3af92b7166a0dfbbfc9fb6b
T_S_V <tsv2013@noreply.github.com>
2022-05-30 16:16:18 +0300

    Resolved #226 - Add paddings to chart labels

--- src/plotly/setup.ts
+++ src/plotly/setup.ts
@@ -216,7 +216,7 @@ export class PlotlySetup {
           return PlotlySetup.getTruncatedLabel(
             label,
             model.options.labelTruncateLength
-          );
+          ) + "  ";
         }),
       },
       xaxis: {
