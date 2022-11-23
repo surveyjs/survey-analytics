365818499f47ee30c61c724468452ff20a2d5679
TSV <tsv2013@users.noreply.github.com>
2022-11-23 16:30:42 +0300

    Added spanish stop words

--- src/wordcloud/stopwords/index.ts
+++ src/wordcloud/stopwords/index.ts
@@ -1,11 +1,13 @@
 import { stopWords } from "./english";
 import { stopWords as noStopWords } from "./norwegian";
 import { stopWords as nlStopWords } from "./dutch";
+import { stopWords as esStopWords } from "./spanish";
 
 var stopWordsDictionary: { [index: string]: Array<string> } = {};
 stopWordsDictionary["en"] = stopWords;
 stopWordsDictionary["no"] = noStopWords;
 stopWordsDictionary["nl"] = nlStopWords;
+stopWordsDictionary["es"] = esStopWords;
 
 export var textHelper = {
   getStopWords: (locale: string = "") => {
