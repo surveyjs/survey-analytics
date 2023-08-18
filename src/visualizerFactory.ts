da1be0c3d20cf7c5a1c9ff042b2a1bb3df9e6e98
Roman Tsukanov <roman.tsukanov@devexpress.com>
2023-08-18 14:08:54 +0400

    Add a code example for VisualizerFactory.createVisualizer (fix #341)

--- src/visualizerFactory.ts
+++ src/visualizerFactory.ts
@@ -7,8 +7,20 @@ import { VisualizationManager } from "./visualizationManager";
  */
 export class VisualizerFactory {
   /**
-   * Creates a visualizer for a specified question.
-   *
+   * Creates a visualizer for a single question.
+   * 
+   * ```js
+   * import { VisualizerFactory } from "survey-analytics";
+   * 
+   * const visualizer = new VisualizerFactory.createVisualizer(
+   *   question,
+   *   data,
+   *   options
+   * );
+   * 
+   * visualizer.render("containerId")
+   * ```
+   * 
    * If a question has more than one [registered](https://surveyjs.io/dashboard/documentation/api-reference/visualizationmanager#registerVisualizer) visualizer, users can switch between them using a drop-down menu.
    * @param question A question for which to create a visualizer.
    * @param data A data array with survey results to be visualized.
