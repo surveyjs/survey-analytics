67d83eadda167f7cfe96e969415deca47e9399ef
Dmitry Kuzin <dk981234@gmail.com>
2025-02-10 19:01:47 +0400

    Fix unit tests

--- jest.config.js
+++ jest.config.js
@@ -2,22 +2,23 @@ module.exports = {
   testEnvironment: "jsdom",
   reporters: [
     "default",
-    [ "jest-junit", {
-    "suiteNameTemplate": "{filepath}",
-    "outputDirectory": ".",
-    "outputName": "junit.xml"
-    } ]
+    ["jest-junit", {
+      "suiteNameTemplate": "{filepath}",
+      "outputDirectory": ".",
+      "outputName": "junit.xml"
+    }]
   ],
   collectCoverage: true,
   coverageReporters: ["json", "lcov", "text", "html", "text-summary", "cobertura"],
   roots: ["tests"],
   transform: {
-    "^.+\\.(ts|tsx)?$": [ "ts-jest", {
+    "^.+\\.(ts|tsx)?$": ["ts-jest", {
       diagnostics: false,
       tsconfig: "tsconfig.test.json"
     }]
   },
   moduleNameMapper: {
+    "svgbundle": "<rootDir>/tests/empty-module.js",
     "\\.(css|scss|html)$": "<rootDir>/tests/empty-module.js"
   },
   testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(ts|tsx)?$",
