ca8981efb5ca266fb4510e54f06cf21890f9ce26
LARINA-NB-W10\OlgaLarina <olga.larina.dev@gmail.com>
2025-06-27 11:09:14 +0300

    update comments

--- src/apexcharts/selectBase.ts
+++ src/apexcharts/selectBase.ts
@@ -65,12 +65,6 @@ export class ApexChartsAdapter {
         ...chartOptions.chart,
         locales: [{
           name: localization.currentLocale,
-          options: {
-            months: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
-            shortMonths: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
-            days: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
-            shortDays: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"]
-          }
         }],
         defaultLocale: localization.currentLocale
       },
