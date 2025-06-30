ec3488b07969f344884c95a83fc13424003efbbf
tsv2013 <tsv2013@noreply.github.com>
2025-06-30 13:00:01 +0300

    Added screenshot tests for charts

--- e2e/helper.ts
+++ e2e/helper.ts
@@ -1,13 +1,19 @@
 import type { Locator, Page } from "@playwright/test";
 import { expect, test as baseTest } from "@playwright/test";
 
-export async function compareScreenshot(page: Page, elementSelector: string | undefined, screenshotName: string, elementIndex = 0) {
+export async function compareScreenshot(page: Page, elementSelector: string | Locator | undefined, screenshotName: string, elementIndex = 0): Promise<void> {
   await page.addStyleTag({
     content: "textarea::-webkit-resizer { visibility: hidden !important; }"
   });
 
   if (!!elementSelector) {
-    const element = page.locator(elementSelector).filter({ visible: true });
+    let elementLocator: Locator;
+    if(typeof elementSelector == "string") {
+      elementLocator = page.locator(elementSelector);
+    } else {
+      elementLocator = elementSelector;
+    }
+    const element = elementLocator.filter({ visible: true });
     await expect.soft(element.nth(elementIndex)).toBeVisible();
     await expect.soft(element.nth(elementIndex)).toHaveScreenshot(screenshotName, {
       timeout: 10000
