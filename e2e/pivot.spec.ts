a0e5104fe0dd8ae1610213f2f7c9fdbe8e3266d4
LARINA-NB-W10\OlgaLarina <olga.larina.dev@gmail.com>
2025-07-02 15:08:49 +0300

    work for #523 fix visual tests

--- e2e/pivot.spec.ts
+++ e2e/pivot.spec.ts
@@ -8,39 +8,39 @@ test.describe("Pivot chart", () => {
   });
 
   test("simple cases", async ({ page }) => {
-    const xAxisSelector = page.locator(".sa-dropdown").nth(0);
-    const yAxisSelector = page.locator(".sa-dropdown").nth(1);
+    const xAxisSelector = page.locator(".sa-dropdown").nth(1);
+    const yAxisSelector = page.locator(".sa-dropdown").nth(2);
     await expect(page.locator("#pivotContainer").getByText("X axis:")).toBeVisible();
     await expect(xAxisSelector).toBeVisible();
     await expect(page.locator("#pivotContainer").getByText("Y axis:")).toBeVisible();
     await expect(yAxisSelector).toBeVisible();
-    // await compareScreenshot(page, "#pivotContainer", "pivot-q1-none.png");
+    await compareScreenshot(page, "#pivotContainer", "pivot-q1-none.png");
 
     await yAxisSelector.click();
     await page.getByRole("list").getByText("Item kind").click();
-    // await compareScreenshot(page, "#pivotContainer", "pivot-q1-q2.png");
+    await compareScreenshot(page, "#pivotContainer", "pivot-q1-q2.png");
     await yAxisSelector.click();
     await page.getByRole("list").getByText("Bill amount").click();
-    // await compareScreenshot(page, "#pivotContainer", "pivot-q1-q3.png");
+    await compareScreenshot(page, "#pivotContainer", "pivot-q1-q3.png");
     await xAxisSelector.click();
     await page.getByRole("list").getByText("Item kind").click();
     await yAxisSelector.click();
     await page.getByRole("list").getByText("Gender").click();
-    // await compareScreenshot(page, "#pivotContainer", "pivot-q2-q1.png");
+    await compareScreenshot(page, "#pivotContainer", "pivot-q2-q1.png");
     await yAxisSelector.click();
     await page.getByRole("list").getByText("Bill amount").click();
-    // await compareScreenshot(page, "#pivotContainer", "pivot-q2-q3.png");
+    await compareScreenshot(page, "#pivotContainer", "pivot-q2-q3.png");
     await xAxisSelector.click();
     await page.getByRole("list").getByText("Bill amount").click();
     await yAxisSelector.click();
     await page.getByRole("list").getByText("Gender").click();
-    // await compareScreenshot(page, "#pivotContainer", "pivot-q3-q1.png");
+    await compareScreenshot(page, "#pivotContainer", "pivot-q3-q1.png");
     await yAxisSelector.click();
     await page.getByRole("list").getByText("Item kind").click();
-    // await compareScreenshot(page, "#pivotContainer", "pivot-q3-q2.png");
+    await compareScreenshot(page, "#pivotContainer", "pivot-q3-q2.png");
     await yAxisSelector.click();
     await page.getByRole("list").getByText("Bill amount").click();
-    // await compareScreenshot(page, "#pivotContainer", "pivot-q3-q3.png");
+    await compareScreenshot(page, "#pivotContainer", "pivot-q3-q3.png");
   });
 
 });
