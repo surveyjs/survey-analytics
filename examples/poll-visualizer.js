fd6af6daf268103946e3d9280f223c4878822a53
LARINA-NB-W10\OlgaLarina <olga.larina.dev@gmail.com>
2025-07-10 15:23:55 +0300

    work for #523 new styles for statistics table

---
+++ examples/poll-visualizer.js
@@ -0,0 +1,47 @@
+// Disable the default Rating Scale question visualizers
+SurveyAnalytics.VisualizationManager.unregisterVisualizer("rating", SurveyAnalytics.HistogramPlotly);
+SurveyAnalytics.VisualizationManager.unregisterVisualizer("rating", SurveyAnalytics.GaugePlotly);
+// Enable the NPS Visualizer for Rating Scale questions
+SurveyAnalytics.VisualizationManager.registerVisualizer("rating", SurveyAnalytics.NpsVisualizer);
+
+var json = {
+  "pages": [
+    {
+      "name": "promotion",
+      "elements": [
+        {
+          "type": "rating",
+          "name": "nps",
+          "title": "How likely are you to recommend our product to a friend or colleague?",
+          "isRequired": true,
+          "rateMin": 0,
+          "rateMax": 10,
+          "minRateDescription": "Most unlikely",
+          "maxRateDescription": "Most likely"
+        }
+      ]
+    }   
+  ]
+};
+var survey = new Survey.SurveyModel(json);
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function generateData() {
+    const data = [];
+    for (let index = 0; index < 1000; index++) {
+        data.push({
+            nps: (index % 2) ? randomIntFromInterval(0, 10) : randomIntFromInterval(8, 10)
+        });
+    }
+    return data;
+}
+var dataFromServer = generateData();
+
+var vizPanel = new SurveyAnalytics.VisualizationPanel(
+    survey.getAllQuestions(),
+    dataFromServer,
+    { allowDynamicLayout: false, allowHideQuestions: false }
+);
+vizPanel.render(document.getElementById("container"));
